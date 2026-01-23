c40e305aea99ba7aaaed053dac1e4edc414c2b46
Dias <seidakhmetdias@gmail.com>
2026-01-23 09:23:58 +0500

    Dias

--- lab1.py/3.py
+++ lab1.py/3.py
@@ -1,8 +1,2 @@
 a=input()
-if a.isdigit():
-    if a[0]==0:
-        print("The type is <class 'str'>")
-    else:
-        print("The type is <class 'int'>")
-else :
-    print("The type is <class 'str'>")
+print("The type is <class 'str'>")
