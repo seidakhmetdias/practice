cab080751252a345114af066e80f6900c3a052ce
Dias <seidakhmetdias@gmail.com>
2026-01-23 09:18:51 +0500

    first commit

---
+++ lab1.py/3.py
@@ -0,0 +1,8 @@
+a=input()
+if a.isdigit():
+    if a[0]==0:
+        print("The type is <class 'str'>")
+    else:
+        print("The type is <class 'int'>")
+else :
+    print("The type is <class 'str'>")
